0fa5ae491cd85974eb0924b3405c1fe250292994
anir183 <188557785+anir183@users.noreply.github.com>
2026-02-02 13:37:29 +0530

    proj(setup): create new vite-react project for frontend

---
+++ frontend/prettier.config.ts
@@ -0,0 +1,13 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import { type Config } from "prettier";
+
+const config: Config = {
+	trailingComma: "es5",
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: false,
+	semi: true,
+};
+
+export default config;
