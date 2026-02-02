0b16578939534893fb582ccedafa00547451904b
anir183 <188557785+anir183@users.noreply.github.com>
2026-02-02 15:43:57 +0530

    proj(setup): create basic nodejs backend project and dev tools

---
+++ backend/prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+	trailingComma: "es5",
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: false,
+	semi: true,
+};
+
+export default config;
